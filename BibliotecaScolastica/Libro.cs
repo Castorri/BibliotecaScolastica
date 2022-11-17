d85b84c63995b3a99a00bb7637ab41648f2c13ce
Lorenzo Castorri <LORENZO.CASTORRI@local.ittcesena.it>
2022-11-17 08:40:32 +0100

    versione 1: proprieta della classe libro

---
+++ BibliotecaScolastica/Libro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaScolastica
+{
+    internal class Libro
+    {
+        internal string Autore { get; set; }
+        internal string Titolo { get; set; }
+        internal int AnnoPubblicazione  { get; set; }
+        internal string Editore { get; set; }
+        internal int NPagine { get; set; }
+
+        internal Libro (string autore, string titolo, int annoPubblicazione, string editore, int nPagine)
+        {
+            this.Autore = autore;
+            this.Titolo = titolo;
+            this.AnnoPubblicazione = annoPubblicazione;
+            this.Editore = editore;
+            this.NPagine = nPagine;
+        }
+    }
+}
